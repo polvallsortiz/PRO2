95f8daf12a11784a3d8d4a7c50dbfbe3668647c3
polvallsortiz <pol.valls.ortiz@est.fib.upc.edu>
2017-01-02 19:07:55 +0100

    Transition MAC-UBUNTU

--- sessio13/X50439_ca-public/main.cc
+++ sessio13/X50439_ca-public/main.cc
@@ -3,5 +3,10 @@
 using namespace std;
 
 int main() {
-
+    Pila<int> c;
+    llegir_pila_int(c,-1);
+    Pila<int> d;
+    llegir_pila_int(d,-1);
+    c = d;
+    escriure_pila_int(c);
 }
--- sessio13/X50439_ca-public/program.hh
+++ sessio13/X50439_ca-public/program.hh
@@ -6,14 +6,22 @@ Pila& operator=(const Pila& original) {
    contingut anterior del p.i. ha estat esborrat (excepte si el
    p.i. i original ja eren el mateix objecte) */
     if(altura == 0) {
-        if(original.altura != 0) {
-
+        if(original.altura != 0) { //PI I ORIGINAL BUIDES
+            primer_node = original.primer_node;
+            altura = original.altura;
         }
-
     }
     else {
-        if(original.altura != 0) {
-
+        if(original.altura == 0) {
+            primer_node = NULL;
+            altura = 0;
+        }
+        else { //NO SON BUIDES
+            if(primer_node != original.primer_node) {
+                esborra_node_pila_it(primer_node);
+                primer_node = original.primer_node;
+                altura = original.altura;
+            }
         }
     }
 }
